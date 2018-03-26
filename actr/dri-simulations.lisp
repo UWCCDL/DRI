6d43e03669d4b63e19561d25f93b94155f9a59c2
TheRealDrDre <stocco@uw.edu>
2018-03-26 12:42:31 -0700

    New simulations

--- actr/dri-simulations.lisp
+++ actr/dri-simulations.lisp
@@ -1,6 +1,8 @@
 (defparameter *buffers* '(visual visual-location retrieval imaginal goal manual)
   "List of ACT-R buffers used by the DRI model")
 
+
+
 (defun simulate-tms-method1 (&optional (verbose t))
   "Simulates the effects of TMS on every buffer used by the model"
   (dolist (buffer *buffers*)
@@ -47,3 +49,25 @@
 		      production))))
 	(save-log filename)))
     (setf *tms-verbose* tmsv)))
+
+
+(defun model-fit (&optional (verbose t))
+  "Grid-search across two parameters"
+  (dolist (p1 '(1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2 2.3))
+    (dolist (p2 '(0.1 0.11 0.12 0.13 0.14 0.15 0.16 0.17 0.18 0.19 0.20))
+      (when verbose
+	(format t "... Simulating Params  ~A ~A~%" p1 p2))
+      (dri-reload)
+      (sgp :v nil) ;; silences the trace
+      (sgp-fct (list :blc p1 :motor-feature-prep-time p2))
+      (setf *tms-method1* nil)
+      (setf *tms-method2* nil)
+      (run 1000 :real-time nil)
+      (let ((filename
+	     (string-downcase
+	      (format nil
+		      "~A-baseline-~A-~A.txt"
+		      (current-model)
+		      p1
+		      p2))))
+	(save-log filename)))))
---
+++ actr/simulations.lisp
@@ -0,0 +1,71 @@
+;;; ==================================================================
+;;; 2AFC SIMULATIONS CODE
+;;; ==================================================================
+
+;;; Params hould be given in the form:
+;;;
+;;;   ((:PARAM_1 VAL) (:PARAM_2 VAL) ... (:PARAM_N VAL))
+;;;
+;;; Note that this is NOT the form in which params are accepted by
+;;; ACT-R functions, like sgp and sgp-fct
+;;;
+;;; Usage:
+;;; ------
+;;;
+;;; (simulate 100 :params '((:ga 2.0) (:le 0.6)) :start 10 :filename "~/Documents/ga2_le06.csv")
+;;;
+(defun simulate (n &key (params nil) (start 0) (filename nil))
+  "A generic function to run the model N times. Returns a table of performance measures with the params"
+  (let ((results nil)
+	(colnames (append (list "idx")
+			  (mapcar #'(lambda (x)
+				      (string-downcase
+				       (format nil "~A" x)))
+				  (mapcar #'first params))
+			  (list "stim" "response" "rt"))))
+					    
+    (dotimes (i n (append (list colnames) (reverse results)))
+      (let ((p (make-instance '2afc-task)))
+	;;(suppress-warnings (reload))
+	(dri-reload p)
+	(sgp :v nil
+	     :style-warnings nil
+	     :model-warnings nil)
+	
+	;; Applies the necessary parameters
+
+	(when params
+	  (sgp-fct (flatten params)))
+
+	;; Do the simulations
+
+	(run 3000)
+
+	;; Create a list of trial-by-trial performance
+	;; (with the corresponding parameter values)
+	
+	(let* ((formatted (extract-results p))
+	       (information (cons (+ start i)
+				  (mapcar #'second params))))
+	  (dolist (trial formatted)
+	    (push (append information trial)
+		  results)))))
+
+    ;; IF a filename is given, write a CSV file
+
+    (let* ((rev (reverse results))
+	   (final-version (push colnames rev)))
+      
+      (if filename
+	  (write-csv final-version filename)
+	  final-version))))
+	
+
+(defun write-csv (table filename)
+  "Writes a list of trials (formatted as in 'extract-results') in a CSV file"
+  (with-open-file (out filename
+		       :direction :output
+		       :if-exists :append
+		       :if-does-not-exist :create)
+    (dolist (row table)
+      (format out "~{~a~^,~}~%" row))))
